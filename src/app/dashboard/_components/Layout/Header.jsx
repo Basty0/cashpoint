8303e3f5ec6936e0cd978a1d7199d8d4876107e4
Basty0 <126388152+Basty0@users.noreply.github.com>
2024-10-07 13:54:41 +0300

    ajout de déconnexion

--- src/app/dashboard/_components/Layout/Header.jsx
+++ src/app/dashboard/_components/Layout/Header.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Bell, CircleUser, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -13,8 +14,15 @@ import {
 import MobileNavigation from "./MobileNavigation";
 import { ModeToggle } from "@/components/Header/ModeToggle";
 import { Badge } from "@/components/ui/badge";
+import { useRouter } from "next/navigation";
+import { logoutUser } from "@/api/api";
 
 export function Header() {
+  const router = useRouter();
+  const logout = async () => {
+    await logoutUser();
+    router.push("/auth/login");
+  };
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40  px-4 lg:h-[60px] lg:px-6">
       <MobileNavigation />
@@ -24,7 +32,7 @@ export function Header() {
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
-              placeholder="Search products..."
+              placeholder="Rechercher des produits..."
               className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
             />
           </div>
@@ -33,17 +41,17 @@ export function Header() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
+            <CircleUser className="h-6 w-6" />
+            <span className="sr-only">Basculer le menu utilisateur</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel>Mon Compte</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Paramètres</DropdownMenuItem>
           <DropdownMenuItem>Support</DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
+          <DropdownMenuItem onClick={logout}>Se déconnecter</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       <ModeToggle />
--- src/app/dashboard/transaction/page.jsx
+++ src/app/dashboard/transaction/page.jsx
@@ -7,9 +7,7 @@ const Page = () => {
   return (
     <div>
       <h1>Trasaction</h1>
-      <Button asChild>
-        <Link href="/dashboard/transaction/123">Voir la transaction </Link>
-      </Button>
+
       <PresPage />
     </div>
   );
